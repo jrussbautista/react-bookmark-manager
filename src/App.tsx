16969b12d4c7feb25b7034e5fa7249d3ab8f17a5
jrussumbrella <jrussclay@gmail.com>
2022-03-26 11:12:22 +0800

    Move App to root src directory

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
-import { store } from './store';
-
-import Routing from '../routes';
-import theme from '../theme';
+import { store } from 'app/store';
+import Routing from 'routes';
+import theme from 'theme';
 
 const App = () => {
   return (
