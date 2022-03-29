cf8a5d9867ac05452d426791e431233e220dccef
jrussumbrella <jrussclay@gmail.com>
2022-03-29 19:14:05 +0800

    Implement view bookmark feature

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
 import Home from 'pages/home';
 import BookmarksPage from 'pages/bookmarks';
 import BookmarkEditPage from 'pages/bookmark-edit';
 import BookmarkAdd from 'pages/bookmark-add';
+import BookmarkDetailsPage from 'pages/bookmark-details';
 import Layout from 'app/Layout';
 
 const Routing = () => {
@@ -15,6 +15,7 @@ const Routing = () => {
           <Route index element={<Home />} />
           <Route path="bookmarks" element={<BookmarksPage />} />
           <Route path="bookmarks/add" element={<BookmarkAdd />} />
+          <Route path="bookmarks/:id" element={<BookmarkDetailsPage />} />
           <Route path="bookmarks/:id/edit" element={<BookmarkEditPage />} />
           <Route path="*" element={<Navigate to="." />} />
         </Route>
